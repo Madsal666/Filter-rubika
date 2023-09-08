fefbe520ac93bcc6d2bd05136f661ded56c2b16a
Madsal <139157200+Madsal666@users.noreply.github.com>
2023-09-08 09:48:56 +0330

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,222 @@
+# madsal
+# fuck all
+print("")
+print("")
+print(" ")
+print("""
+__  __    _    ____
+|  \/  |  / \  |  _ \
+| |\/| | / _ \ | | | |
+| |  | |/ ___ \| |_| |
+|_|  |_/_/   \_\____/""")
+print(" ")
+print("""
+ __    __    __
+ / /_  / /_  / /_
+| '_ \| '_ \| '_ \
+| (_) | (_) | (_) |
+ \___/ \___/ \___/ """)
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      mad filtering")
+print (Fore.YELLOW + "")
+x = """ Loading ~ ~ ~ 
+
+
+
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀.　　　　　　　　　　　　　.　　　ﾟ .　　　　　　　　　　　　　. 　　　　　　　　　　　　　　　✦ 　　　　　,　　　　　　　.
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀☀️
+　　　　　　*　　　　　　　　　　　.
+.　　　　　　　　　　　　. 　　✦⠀　   　　　,　　　　　　　　　*
+　　　　🛸　⠀　　　　⠀　　,
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀.　　　
+　　 　　⠀　　　⠀.　
+　　˚　　　⠀　⠀  　　,　　　　　　.
+　　　　　　　　　　　　　.
+　　　　　　*⠀　　⠀  　　　　　⠀✦⠀　
+　　　　　　　　　　　　　　　　　　.
+　　　　.　　　　.　　　⠀🌕
+　　　　　　　　　　　.
+　　　　　　　🚀
+　　　˚　　　　　　　　ﾟ　　　　　.
+　.⠀　　🌎⠀‍⠀‍⠀‍⠀‍⠀‍⠀‍⠀‍⠀‍⠀‍⠀‍⠀,
+　　🛰️ . 　　✦⠀　   　　　,　　　
+
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print(f"{red} ")
+d = ("""
+
+ __  __           _ _   _            _
+|  \/  | __ _  __| | | | | __ _  ___| | __
+| |\/| |/ _` |/ _` | |_| |/ _` |/ __| |/ /
+| |  | | (_| | (_| |  _  | (_| | (__|   <
+|_|  |_|\__,_|\__,_|_| |_|\__,_|\___|_|\_\
+
+madfiltering
+            
+
+join us""")
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+#  * _ * 
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("Play - - - ! ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       teleg - @hack666m")
+print(" = madhack -- ")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+
+[channel (1) ]
+[ group  (2) ]
+                 
+[Account (3) ]
+                  
+[super code (0) ]
+
+[Exit    (4) ]
+
+[report acc (5) ]
+
+[Code Rip (6) ]
+
+[ account onfilter (7) ]
+
+Which do you want ?""")
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" 
+_____________
+
+Loading . . . 
+ 
+👺👺👺👺👺👺👺👺👺👺
+
+                    
+    
+|||||Telegram: @hack666m ||||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
+_______________________
+""")
+print(f"{red} ")
+
+if up == "7":
+    os.system("python sms.py")
+if up == "6":
+    os.system("python report.py")
+if up == "5":
+    os.system("python pass.py")
+if up == "4":
+    os.system("exit 5")
+    
+if up == "0":
+         print ('(Super) Code filter =>' + '%s%s  (/5.6.7.filtering_rubika///6.5.3.8.h//310.561///5.1.1.6) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code Fil Channel :' + '%s%s (7.8.9.2.d/f///y//6065///g.k.4.6.7.8.2.3.5.6.7.3.2.9.3.9.1.5.3.6.2.9.6/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "1":
+        print("    Code  ")
+
+
+if up == "2":
+        print ('code group :' + '%s%s (4.0.1.4/f.i.d/gl/4.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "2":
+
+        print("     Code")
+
+
+if up == "3":
+
+        print ('code Filter account :' + '%s%s (6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "3":
+        print("""
+        
+             🗿""")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""madsal""")
+print(f"{blue}" )
+print("")
+print(" Chanel Telegram : @hack666m -")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print(" fuck love ")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s thanks for using  %s'  % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print(" ")
+print(" ")
+print(" ")
+print ("!")
+print("")
+print(""" (Update script) 👍🏿:""")
+print("")
+print("")
+print("         </madhack>")
+print ("")
+time.sleep(0.3)
+print(f"{red}" )
+time.sleep(1)
+print ("""!""")
+print("")
+print (Fore.WHITE + "")
